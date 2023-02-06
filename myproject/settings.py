eb077b3d17e5c1672e31107b096486a916a98076
ThaoThaoHeart <jennyhoang_9@hotmail.com>
2023-02-06 10:00:21 -0500

    remote key

--- myproject/settings.py
+++ myproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--p8o7=f_g6ypr3y-@$iy0*30440o7=g1(y#4^0lkxehg*bhk5l'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
